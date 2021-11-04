ead891c86ec1fd0c524477f0af66ba8fdb16b4bc
yamagashi <90173762+yamagashi@users.noreply.github.com>
2021-11-04 15:23:16 +0900

    commit

--- Sample1/src/main/java/model/Login.java
+++ Sample1/src/main/java/model/Login.java
@@ -1,13 +1,17 @@
-/**
- * 
- */
+
 package model;
 
-/**
- * @author yamagashi
- * 
- *
- */
 public class Login {
+	private String userId;
+	private String pass;
+
+	public Login(String userId,String pass) {
+		this.userId = userId;
+		this.pass = pass;
+	}
+
+	public String getUserId() {return userId;}
+	public String getpass() {return pass;}
+
 
 }
