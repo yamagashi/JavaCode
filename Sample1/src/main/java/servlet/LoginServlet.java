3abe8401dbd33d7fad3258b6e3096173c4eef918
yamagashi <90173762+yamagashi@users.noreply.github.com>
2021-11-06 13:09:39 +0900

    commit

---
+++ Sample1/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatchar = request.getRequestDispatcher(
+				"/WEB-INF/jsp/login.jsp");
+		dispatchar.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatchar = request.getRequestDispatcher(
+				"/WEB-INF/jsp/loginOK.jsp");
+		dispatchar.forward(request, response);
+	}
+
+}
---
+++ Sample1/src/main/java/servlet/WelcomServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomServlet
+ */
+@WebServlet("/WelcomServlet")
+public class WelcomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher dispatchar = request.getRequestDispatcher(
+				"/WEB-INF/jsp/welcom.jsp");
+		dispatchar.forward(request, response);
+	}
+
+}
